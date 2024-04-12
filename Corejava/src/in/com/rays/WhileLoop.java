36d084c0f1d83fefde76b0b7c75cffc6a6bae1c2
Rahul kannavedia <149078010+rahulkannavedia@users.noreply.github.com>
2024-04-12 16:08:38 +0530

    Update WhileLoop.java

--- Corejava/src/in/com/rays/WhileLoop.java
+++ Corejava/src/in/com/rays/WhileLoop.java
@@ -3,9 +3,9 @@ package in.com.rays;
 public class WhileLoop {
 public static void main (String[]args) {
 	int i = 1;
-			while (i < 10) {
-				System.out.println(i);
-				i++;
+	while (i < 10) {
+	System.out.println(i);
+	i++;
 				
 			}
 			
